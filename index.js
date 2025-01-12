a0da7ca534584dd5f655fc46097e8cbc8dec680e
Vikash Mandal <aaryavikash161gmail.com>
2025-01-12 19:50:18 +0530

    first commit

--- index.js
+++ index.js
@@ -20,12 +20,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
-
-const corsOptions = {
-    origin: process.env.URL,
-    credentials: true
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 // yha pr apni api ayengi
 app.use("/api/v1/user", userRoute);
